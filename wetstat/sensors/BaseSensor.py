148035f132a4476ea01945e134b3603092af87eb
bb1950328 <bb1950328@gmail.com>
2019-02-06 19:55:19 +0100

    created sensor classes

---
+++ wetstat/sensors/BaseSensor.py
@@ -0,0 +1,11 @@
+class BaseSensor:
+    def __init__(self):
+        raise RuntimeWarning("you should not instanciate this class directly!")
+
+    def get_info(self):
+        try:
+            return {"long_name": self.get_long_name(),
+                    "short_name": self.get_short_name(),
+                    "color": self.get_display_color()}
+        except NameError:
+            raise NotImplementedError("child class hasn't defined one of the info methods correctly!")
---
+++ wetstat/sensors/TempSensor.py
@@ -0,0 +1,19 @@
+from wetstat.sensors.BaseSensor import BaseSensor
+
+
+class TempSensor(BaseSensor):
+
+    def __init__(self, number):
+        self.number = number
+
+    def get_long_name(self):
+        return "Temperatur " + self.number
+
+    def get_short_name(self):
+        return "Temp" + self.number
+
+    def get_display_color(self):
+        n = str(hex(self.number + 210))
+        if len(n) == 1:
+            n = "0" + n
+        return "#3875" + n
