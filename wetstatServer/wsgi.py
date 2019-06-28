dba209be308c75162f7593459d035e9c4ac999ad
bb1950328 <bb1950328@gmail.com>
2019-06-28 19:33:19 +0200

    - configuring apache

--- wetstatServer/wsgi.py
+++ wetstatServer/wsgi.py
@@ -8,10 +8,33 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
+# import os
+#
+# from django.core.wsgi import get_wsgi_application
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wetstatServer.settings')
+#
+# application = get_wsgi_application()
+
 import os
+import signal
+import sys
+import time
+import traceback
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wetstatServer.settings')
+sys.path.append('/home/pi/wetstatServer')
+# adjust the Python version in the line below as needed
+sys.path.append('/home/pi/wetstatServer/venv/lib/python3.7/site-packages')
 
-application = get_wsgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wetstatServer.settings")
+
+try:
+    application = get_wsgi_application()
+except Exception:
+    # Error loading applications
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
