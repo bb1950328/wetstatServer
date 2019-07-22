7090e6e8998d3ced8914fc3f6dd00464270959a4
bb1950328 <bb1950328@gmail.com>
2019-07-22 17:05:53 +0200

    - implemented communication with new light sensor (BH1750FVI)

--- wetstat/hardware/sensors/light_sensor.py
+++ wetstat/hardware/sensors/light_sensor.py
@@ -1,11 +1,21 @@
 # coding=utf-8
-from wetstat.hardware.sensors.analog_sensor import AnalogSensor
+from wetstat.hardware.sensors.base_sensor import BaseSensor
+from wetstat.hardware.sensors.bh1750fvi import BH1750FVI, Const
+
+DARK_THRESHOLD = 100
+LIGHT_THRESHOLD = 40_000
+DARK = "DARK"
+NORMAL = "NORMAL"
+LIGHT = "LIGHT"
 
 
-class LightSensor(AnalogSensor):
-    def __init__(self, channel: int = 2) -> None:
+class LightSensor(BaseSensor):
+
+    def __init__(self) -> None:
         super().__init__()
-        self.channel = channel
+        self.bh1750 = BH1750FVI()
+        self.bh1750.set_mode(Const.Opecode.Continious.H_RES)
+        self.mode = NORMAL
 
     def get_long_name(self) -> str:
         return "Licht"
@@ -19,5 +29,30 @@ class LightSensor(AnalogSensor):
     def get_unit(self) -> str:
         return "Lux"
 
-    def measure(self) -> float:
-        return 9.5785 * self.get_bits(self.channel) - 2025.8
+    def change_mode(self, new_mode):
+        if self.mode == new_mode:
+            return
+        if new_mode == DARK:
+            self.bh1750.set_mode(Const.Opecode.Continious.H2_RES)
+            self.bh1750.set_mtreg(Const.MTreg.MT_MAX)
+        else:
+            if self.mode == DARK:
+                self.bh1750.set_mode(Const.Opecode.Continious.H_RES)
+            if new_mode == NORMAL:
+                self.bh1750.set_mtreg(Const.MTreg.MT_DEFAULT)
+            else:  # new_mode == LIGHT
+                self.bh1750.set_mtreg(Const.MTreg.MT_MIN)
+
+    def measure(self, check_mode=True) -> float:
+        value = self.bh1750.get_result()
+        if check_mode:
+            if value < DARK_THRESHOLD and self.mode != DARK:
+                self.change_mode(DARK)
+                return self.measure(check_mode=False)
+            elif value > LIGHT_THRESHOLD and self.mode != LIGHT:
+                self.change_mode(LIGHT)
+                return self.measure(check_mode=False)
+            elif DARK_THRESHOLD <= value <= LIGHT_THRESHOLD and self.mode != NORMAL:
+                self.change_mode(LIGHT)
+                return self.measure(check_mode=False)
+        return value
