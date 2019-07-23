b6a0c81aaa918b72074b990f7a50ac4908a83a40
bb1950328 <bb1950328@gmail.com>
2019-07-23 19:59:41 +0200

    -

--- test/test_bh1750fvi.py
+++ test/test_bh1750fvi.py
@@ -5,6 +5,10 @@ from wetstat.hardware.sensors.light_sensor import LightSensor
 
 ls = LightSensor()
 
+x = False
+
 while True:
-    print(f"\rValue: {ls.measure()} lux, mode is {ls.mode}", end="")
+    print("\r", " " * 80, end="")
+    print(f"\rValue: {round(ls.measure(), 3)} lux, mode is {ls.mode} {' .' if x else '. '}", end="")
+    x = not x
     time.sleep(0.5)
